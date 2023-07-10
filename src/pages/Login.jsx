1b552b12da11a842cb431c40907617a4b8e22e93
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2023-07-10 13:25:14 +0300

    fake auth added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
+import Button from "../components/Button";
 
 export default function Login() {
+  const navigate = useNavigate();
+
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+
+  const { isAuthenticated, login } = useAuth();
+
+  function handleLogin(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app", { replace: true });
+    },
+    [isAuthenticated, navigate]
+  );
 
   return (
     <main className={styles.login}>
@@ -32,7 +51,9 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button onClick={handleLogin} type="primary">
+            Login
+          </Button>
         </div>
       </form>
     </main>
